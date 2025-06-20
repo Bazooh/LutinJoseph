28921361905d001a5aeec98bccf1fc86652e62fa
Aymeric Conti <aymeric.conti2002@gmail.com>
2025-06-20 20:22:14 +0200

    typescript

---
+++ app.ts
@@ -0,0 +1,13 @@
+import { createApp, ref } from 'vue'
+
+const App = {
+    setup() {
+        const message = ref('Test haha')
+        const reverseMessage = () => {
+            message.value = message.value.split('').reverse().join('')
+        }
+        return { message, reverseMessage }
+    }
+}
+
+createApp(App).mount('#app')
